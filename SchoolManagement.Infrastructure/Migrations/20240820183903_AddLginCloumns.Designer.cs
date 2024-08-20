86c40d2e9006f70e3c512603d00b4215e6c840e3
hozenyth.andrade <hozenyth.andrade@gmail.com>
2024-08-20 15:44:04 -0300

    updating migrations and add columns password and role in students.

--- SchoolManagement.Infrastructure/Migrations/20240820183903_AddLginCloumns.Designer.cs
+++ SchoolManagement.Infrastructure/Migrations/20240820183903_AddLginCloumns.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SchoolManagement.Infrastructure.Persistence.Repositories;
 
@@ -11,9 +12,10 @@ using SchoolManagement.Infrastructure.Persistence.Repositories;
 namespace SchoolManagement.Infrastructure.Migrations
 {
     [DbContext(typeof(SchoolManagementDbContext))]
-    partial class SchoolManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240820183903_AddLginCloumns")]
+    partial class AddLginCloumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SchoolManagement.Infrastructure/Migrations/20240820183903_AddLginCloumns.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolManagement.Infrastructure.Migrations
+{
+    public partial class AddLginCloumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TeacherId",
+                table: "Courses",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Students");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TeacherId",
+                table: "Courses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
