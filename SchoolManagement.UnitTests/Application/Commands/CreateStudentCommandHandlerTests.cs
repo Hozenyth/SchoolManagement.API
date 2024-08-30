7b849ee18cbc2860de0f3a3db8b00f1aaa4813b5
hozenyth.andrade <hozenyth.andrade@gmail.com>
2024-08-30 17:14:26 -0300

    Done teste with X-Unit in application Create Student and Teacher.

---
+++ SchoolManagement.UnitTests/Application/Commands/CreateStudentCommandHandlerTests.cs
@@ -0,0 +1,40 @@
+﻿using Moq;
+using SchoolManagement.Application.Comands.CreateStudent;
+using SchoolManagement.Core.Entities;
+using SchoolManagement.Core.Repositories;
+using SchoolManagement.Core.Services;
+
+namespace SchoolManagement.UnitTests.Application.Commands
+{
+    public class CreateStudentCommandHandlerTests
+    {
+        [Fact]
+        public async Task InputDataIsOk_Executed_ReturnStudentId()
+        {
+            //Arrange
+            var studentRepository = new Mock<IStudentRepository>();
+            var authService = new Mock<IAuthService>();
+
+            var createStudentCommand = new CreateStudentCommand
+            {
+                Email = "teste@teste.com",
+                Name = "name",
+                PhoneNumber = "1234567890",
+                Registration = 1,
+                Password = "password",
+                Role = "student"               
+            };
+          
+            var createStudentCommandHandler = new CreateStudentCommandHandler(studentRepository.Object, authService.Object);
+
+            //Act
+
+            var id = await createStudentCommandHandler.Handle(createStudentCommand, new CancellationToken());
+
+            //Assert
+
+            Assert.True(id >= 0);
+            studentRepository.Verify(c => c.AddAsync(It.IsAny<Student>()), Times.Once);
+        }
+    }
+}
---
+++ SchoolManagement.UnitTests/Application/Commands/CreateTeacherCommandHandlerTests.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using SchoolManagement.Application.Comands.CreateCourse;
+using SchoolManagement.Application.Comands.CreateStudent;
+using SchoolManagement.Application.Comands.CreateTeacher;
+using SchoolManagement.Core.Entities;
+using SchoolManagement.Core.Repositories;
+using SchoolManagement.Infrastructure.Persistence.Repositories;
+
+namespace SchoolManagement.UnitTests.Application.Commands
+{
+    public class CreateTeacherCommandHandlerTests
+    {
+        [Fact]
+        public async Task InputDataIsOk_Executed_ReturnCourseId()
+        {
+            //Arrange
+            var teacherRepository = new Mock<ITeacherRepository>();
+
+            var createTeacherCommand = new CreateTeacherCommand
+            {
+                Email = "teste@teste.com",
+                Name = "name",
+                PhoneNumber = "1234567890",
+                Registration = 1              
+            };
+
+            var createTeacherCommandHandler = new CreateTeacherCommandHandler(teacherRepository.Object);
+
+            //Act
+
+            var id = await createTeacherCommandHandler.Handle(createTeacherCommand, new CancellationToken());
+
+            //Assert
+
+            Assert.True(id >= 0);
+            teacherRepository.Verify(c => c.AddAssync(It.IsAny<Teacher>()), Times.Once);
+        }
+    }
+}
